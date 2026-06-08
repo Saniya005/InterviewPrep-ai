4fe17d8bd4d07633e427100b5d067d1baddc53d1
Saniya Rathore <saniyarathore005@gmail.com>
2026-06-09 01:53:52 +0530

    fix cookie settings for production

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -42,10 +42,10 @@ async function registerUserController(req, res) {
         { expiresIn: "1d" }
     )
 
-    res.cookie("token", token, {
+   res.cookie("token", token, {
     httpOnly: true,
-    secure: false,
-    sameSite: "lax",
+    secure: true,
+    sameSite: "none",
     maxAge: 24 * 60 * 60 * 1000
 })
 
@@ -93,12 +93,13 @@ async function loginUserController(req, res) {
         { expiresIn: "1d" }
     )
 
-    res.cookie("token", token, {
+   res.cookie("token", token, {
     httpOnly: true,
-    secure: false,
-    sameSite: "lax",
+    secure: true,
+    sameSite: "none",
     maxAge: 24 * 60 * 60 * 1000
 })
+
     res.status(200).json({
         message: "User loggedIn successfully.",
         user: {
